14e2d939fb784ac642d33dea4140d8339f7c6682
Raisa-A <raisa411@gmail.com>
2022-11-15 11:32:21 -0500

    added counter and state

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  
   return (
     <>
       <div className="App-header">
         <h1> Hello, my name is Raisa Ahmed</h1>
       </div>
+      <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>add 1 </button>
+    </div>
     </>
   );
 }
